bbb1b23f73d3c3859b6d6e1e5f837ac326487bc0
taylor-tokareff <taylortokareff@gmail.com>
2021-05-11 09:20:42 -0700

    more pathing

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,9 @@
 import { Component } from 'react';
-import Header from './Header';
+import Header from './HeaderTemp';
 import Footer from './FooterTemp';
-import Home from '../home/Home';
-import CharPage from '../characters/CharPage';
-import CharDetailPage from '../character/CharDetailPage';
+import Home from '../home/HomeTemp';
+import CharPage from '../characters/CharPageTemp';
+import CharDetailPage from '../character/CharDetailPageTemp';
 import {
   BrowserRouter as Router,
   Route,
