5f77322a33f79d234223fd87fede54131588d1b5
taylor-tokareff <taylortokareff@gmail.com>
2021-05-11 14:23:00 -0700

    link 2.0

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 class Header extends Component {
@@ -8,8 +9,10 @@ class Header extends Component {
       <header className="Header">
 
         <h1>Mortal Kombat App</h1>
-        <a href="https://609aee063afb0000089ec4f1--silly-chandrasekhar-6f8e13.netlify.app/">Home Page</a>
-        <a href="https://609aee063afb0000089ec4f1--silly-chandrasekhar-6f8e13.netlify.app/characters">Character List</a>
+        <Link to="/">Home Page</Link>
+        <Link to="/characters">Character List</Link>
+
+
 
       </header>
     );
